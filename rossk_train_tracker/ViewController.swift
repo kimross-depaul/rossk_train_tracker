b9a638778d9c309f4a8516c4407c24efd8024bd5
Kim Ross <kim.ross@lz95.org>
2021-04-28 13:57:01 -0500

    Initial Commit

---
+++ rossk_train_tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rossk_train_tracker
+//
+//  Created by Kim Ross on 4/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
