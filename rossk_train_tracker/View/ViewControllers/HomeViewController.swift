120bae7005dff82103c6ada42531e85520de29d8
Kim Ross <kim.ross@lz95.org>
2021-04-28 16:47:05 -0500

    Initial commit - blank story board and file structure, images

---
+++ rossk_train_tracker/View/ViewControllers/HomeViewController.swift
@@ -0,0 +1,74 @@
+//
+//  HomeViewController.swift
+//  rossk_train_tracker
+//
+//  Created by Kim Ross on 4/28/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet var btnRed: UIButton!
+    @IBOutlet var btnOrange: UIButton!
+    @IBOutlet var btnGreen: UIButton!
+    @IBOutlet var btnYellow: UIButton!
+    @IBOutlet var btnBlue: UIButton!
+    @IBOutlet var btnBrown: UIButton!
+    @IBOutlet var btnPurple: UIButton!
+    @IBOutlet var btnPink: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        btnRed.styleMe();
+        btnOrange.styleMe();
+        btnGreen.styleMe();
+        btnYellow.styleMe();
+        btnBlue.styleMe();
+        btnBrown.styleMe();
+        btnPurple.styleMe();
+        btnPink.styleMe();
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+extension UIButton {
+
+    func styleMe(padding: CGFloat = 0.0) {
+        self.setTitleColor(UIColor.black, for: UIControl.State.normal);
+        let imageSize = self.imageView!.image!.size; //self.imageView!.frame.size
+        let titleSize = self.titleLabel!.frame.size
+        let test = self.bounds.height;
+        let test2 = self.frame.height;
+        let test3 = self.imageView!.frame.width;
+        let rect = self.imageView!.frame;
+        let rect2 = self.imageView!.bounds;
+        let totalHeight = imageSize.height + titleSize.height + padding
+        
+        self.layer.cornerRadius = 15.0;
+
+        self.imageEdgeInsets = UIEdgeInsets(
+            top: -(totalHeight - imageSize.height),
+            left: 0,
+            bottom: 0,
+            right: -titleSize.width
+        )
+        self.titleEdgeInsets = UIEdgeInsets(
+            top: 0,
+            left: -imageSize.width,
+            bottom: -(totalHeight - titleSize.height),
+            right: 0
+        )
+    }
+
+}
